c6189913dee98a337c5824856bea6f9f1188c849
kimilguk <boramcom@daum.net>
2022-02-08 13:07:44 +0900

    5주차 1교시 작업소스

--- src/main/java/com/herokuapp/kimilgukboot2/util/CustomErrorController.java
+++ src/main/java/com/herokuapp/kimilgukboot2/util/CustomErrorController.java
@@ -9,14 +9,18 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CustomErrorController implements ErrorController {
-	@RequestMapping(value="/error")
+	//@RequestMapping(value="/error", method= {RequestMethod.GET})
+	//스프링4.3버전 이전에 사용하던 방식이다. 보통 스프링 부트에서는 @GetMapping("/error") 으로…
+	@GetMapping("/error")
 	public String handleError(HttpServletRequest request, Model model) {
 		
-		//에러코드를 가져온다.
+		//HttpServletRequest 클래스객체에서 에러와 이전 경로를 포함하는 http전송상태 정보를 가진다
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 		//에러코드에 매칭되는 상세정보를 가져온다.
 		HttpStatus httpStatus = HttpStatus.valueOf(Integer.valueOf(status.toString()));
