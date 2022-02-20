96007ab94db01834b5fc595e7e629849511255d2
kimilguk <boramcom@daum.net>
2022-02-20 16:47:43 +0900

    10주차 2교시 작업소스

--- src/main/java/com/herokuapp/kimilgukboot2/util/ScriptUtils.java
+++ src/main/java/com/herokuapp/kimilgukboot2/util/ScriptUtils.java
@@ -19,4 +19,18 @@ public class ScriptUtils {
 		out.println("<script>alert('"+alertText+"');location.href='"+nextPage+"';</script>");
 		out.flush();//객체삭제
 	}
+	//메시지 출력 후 페이지 이동없는 처리(아래)
+		public static void alert(HttpServletResponse response, String alertText) throws IOException {
+			init(response);
+			PrintWriter out = response.getWriter();//스트림 방식으로 출력 객체생성:예외처리필수
+			out.println("<script>alert('"+alertText+"');</script>");
+			out.flush();//객체삭제
+		}
+		//메시지 출력 후 페이지 이동없는 처리(아래)
+		public static void alertAndBackPage(HttpServletResponse response, String alertText) throws IOException {
+			init(response);
+			PrintWriter out = response.getWriter();//스트림 방식으로 출력 객체생성:예외처리필수
+			out.println("<script>alert('"+alertText+"');history.go(-1)</script>");
+			out.flush();//객체삭제
+		}
 }
--- src/main/java/com/herokuapp/kimilgukboot2/web/SimpleUsersController.java
+++ src/main/java/com/herokuapp/kimilgukboot2/web/SimpleUsersController.java
@@ -1,5 +1,9 @@
 package com.herokuapp.kimilgukboot2.web;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -16,6 +20,7 @@ import com.herokuapp.kimilgukboot2.domain.posts.Posts;
 import com.herokuapp.kimilgukboot2.domain.simple_users.SimpleUsers;
 import com.herokuapp.kimilgukboot2.service.posts.PostsService;
 import com.herokuapp.kimilgukboot2.service.simple_users.SimpleUsersService;
+import com.herokuapp.kimilgukboot2.util.ScriptUtils;
 import com.herokuapp.kimilgukboot2.web.dto.SimpleUsersDto;
 
 import lombok.RequiredArgsConstructor;
@@ -33,9 +38,20 @@ public class SimpleUsersController {
 		return "simple_users/save";//save.mustache 생략
 	}
 	@PostMapping("/simple_users/save")//회원생성 API실행
-	public String simpleUsersSavePost(SimpleUsersDto requestDto) {
-		simpleUsersService.save(requestDto);
-		return "redirect:/simple_users/list";//저장 후 절대경로로 페이지이동
+	public String simpleUsersSavePost(HttpServletResponse response,SimpleUsersDto requestDto) throws IOException {
+		SimpleUsersDto usersDto = null;
+		try {
+		usersDto = simpleUsersService.findByName(requestDto.getUsername());
+		}catch(Exception e){
+		}
+		if(usersDto == null) {
+			simpleUsersService.save(requestDto);
+			ScriptUtils.alertAndMovePage(response, "저장 되었습니다.", "/simple_users/list");
+		}else {
+			ScriptUtils.alertAndBackPage(response, "중복 아이디가 존재합니다. 아이디를 다시 입력해 주세요.");
+		}
+		
+		return null;//"redirect:/simple_users/list";//저장 후 절대경로로 페이지이동
 	}
 	@GetMapping("/simple_users/list")//회원목록 디자인보기
 	public String simpleUsersList(@PageableDefault(size=5,sort="id",direction=Sort.Direction.DESC) Pageable pageable, Model model,@LoginUser SessionUser user) {
@@ -59,13 +75,15 @@ public class SimpleUsersController {
 		return "simple_users/update";
 	}
 	@PostMapping("/simple_users/update")//회원수정 API실행
-	public String simpleUsersUpdatePost(SimpleUsersDto requestDto) {
+	public String simpleUsersUpdatePost(HttpServletResponse response,SimpleUsersDto requestDto) throws IOException {
 		simpleUsersService.update(requestDto.getId(), requestDto);
-		return "redirect:/simple_users/update/"+requestDto.getUsername();
+		ScriptUtils.alertAndMovePage(response, "수정 되었습니다.", "/simple_users/update/"+requestDto.getUsername());
+		return null;//"redirect:/simple_users/update/"+requestDto.getUsername();
 	}
 	@PostMapping("/simple_users/delete")//회원삭제 API실행
-	public String simpleUsersDelete(SimpleUsersDto requestDto) {
+	public String simpleUsersDelete(HttpServletResponse response,SimpleUsersDto requestDto) throws IOException {
 		simpleUsersService.delete(requestDto.getId());
-		return "redirect:/simple_users/list";//삭제 후 절대경로로 페이지 이동
+		ScriptUtils.alertAndMovePage(response, "삭제 되었습니다.", "/simple_users/list");
+		return null;//"redirect:/simple_users/list";//삭제 후 절대경로로 페이지 이동
 	}
 }
