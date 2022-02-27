1045528412218d0c883d91f7f5cd4e95b052cdb4
kimilguk <boramcom@daum.net>
2022-02-27 17:03:05 +0900

    12주차 2교시 작업소스

--- src/main/java/com/herokuapp/kimilgukboot2/web/IndexController.java
+++ src/main/java/com/herokuapp/kimilgukboot2/web/IndexController.java
@@ -1,5 +1,14 @@
 package com.herokuapp.kimilgukboot2.web;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.json.JSONObject;
+import org.json.XML;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
@@ -11,6 +20,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.herokuapp.kimilgukboot2.config.auth.LoginUser;
 import com.herokuapp.kimilgukboot2.config.auth.dto.SessionUser;
 import com.herokuapp.kimilgukboot2.domain.posts.Posts;
@@ -30,9 +41,39 @@ public class IndexController {
 	private final FileService fileService;//생성자로 주입
 	
 	@GetMapping("/kakaomap")
-	public String kakaoMap(Model model) {
+	public String kakaoMap(Model model) throws IOException {
 		//공공데이터포털에서 한국전력공사_전기차 충전소 데이터를 받아서 model객체에 담는 코딩예정
-		model.addAttribute("response", null);
+		StringBuilder urlBuilder = new StringBuilder("http://data.uiryeong.go.kr/rest/uiryeongelctyvhclechrstn/getUiryeongelctyvhclechrstnList"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=PLJPmKeBFGOkoxgAoLJgT962Uh0QPWijxPNQ%2Bl%2B4o24r9R%2BqbclT0Fc9xSamDrGiMYAF4CrpJLaDOsKZ%2FDoN%2Bw%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*페이지 크기(기본10)*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*시작 페이지(기본1)*/
+        urlBuilder.append("&" + URLEncoder.encode("chrstn_nm","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*충전소명*/
+        URL url = new URL(urlBuilder.toString());
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        System.out.println("Response code: " + conn.getResponseCode());
+        BufferedReader rd;
+        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+            rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        } else {
+            rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+        }
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = rd.readLine()) != null) {
+            sb.append(line);
+        }
+        rd.close();
+        conn.disconnect();
+        JSONObject jsonObject = XML.toJSONObject(sb.toString());
+        //System.out.println(jsonObject.toString());//xml to json 전체 데이터
+        JSONObject rfcOpenApi = (JSONObject) (jsonObject.get("rfcOpenApi"));
+        JSONObject header = (JSONObject) rfcOpenApi.get("header");
+        JSONObject body = (JSONObject) rfcOpenApi.get("body");
+        //System.out.println(header.toString());//헤더 정보 확인
+        System.out.println(body.toString());//실제 데이터 정보 확인
+		model.addAttribute("response", body);
 		return "kakaomap";//resource루트의 templates폴더에 kakaomap.mustache 파일과 연결
 	}
 	@GetMapping("/posts/update/{id}")
