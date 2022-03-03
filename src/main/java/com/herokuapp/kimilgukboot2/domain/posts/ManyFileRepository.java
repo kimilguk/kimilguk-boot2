56d285638c7f97309674faa018df244edbb1baff
kimilguk <boramcom@daum.net>
2022-03-03 18:14:29 +0900

    13주차 3교시 작업소스

--- src/main/java/com/herokuapp/kimilgukboot2/domain/posts/ManyFileRepository.java
+++ src/main/java/com/herokuapp/kimilgukboot2/domain/posts/ManyFileRepository.java
@@ -11,4 +11,5 @@ public interface ManyFileRepository extends JpaRepository<ManyFile, Long> {
 	//단, 관계형 쿼리는 기본 CRUD메소드에 없으므로, 개발자가 아래처럼 생성해야 한다.
 	@Query(value = "SELECT * FROM many_file p where p.posts_id = :posts_id ORDER BY p.id DESC", nativeQuery = true)
     List<ManyFile> fileAllDesc(@Param("posts_id") Long id);
+	//native쿼리는 위 처럼 파라미터가 필요한 쿼리가 사용가능하고, 주로 다중 파라미터값을 쿼리에 바인딩 할 때 직관적인 코딩으로 많이 사용한다.
 }
